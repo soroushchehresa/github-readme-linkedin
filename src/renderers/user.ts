509ac8cf91addd85be900ce16ef5fc4a95c6e7c9
soroushchehresa <s1996ch@gmail.com>
2020-07-27 20:05:52 +0430

    Update user info styles

--- src/renderers/user.ts
+++ src/renderers/user.ts
@@ -18,7 +18,7 @@ export default class UserInfo {
   renderList(user: User[]): string {
     return `
       <text x="10" y="0">
-        <tspan x="0" dy="25" font-size="26" font-weight="bold" fill="#000" fill-opacity=".9">${parseString(`${_.get(user, 'firstName')} ${_.get(user, 'lastName')}`)}</tspan>
+        <tspan x="0" dy="25" font-size="32" font-weight="bold" fill="#000" fill-opacity=".9">${parseString(`${_.get(user, 'firstName')} ${_.get(user, 'lastName')}`)}</tspan>
         <tspan x="0" dy="30" font-size="18" font-weight="bold" fill="#000" fill-opacity=".6">${parseString(_.get(user, 'headline'))}</tspan>
         <tspan x="0" dy="25" font-size="16" fill="#000" fill-opacity=".6">${parseString(_.get(user, 'location'))}</tspan>
       </text>
