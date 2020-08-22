da2b440223e61f73ab2778a42d80be8c73cc5c02
Soroush Chehresa <s1996ch@gmail.com>
2020-08-22 16:12:50 +0430

    Update index.ts

--- src/images-renderer/routes/index.ts
+++ src/images-renderer/routes/index.ts
@@ -1,5 +1,5 @@
 import { NowRequest, NowResponse } from '@vercel/node';
 
 export default async (req: NowRequest, res: NowResponse) => {
-  res.send('<h3>Go to <a href="https://github.com/soroushchehresa/github-readme-linkedin">github-readme-linkedin</a> for more details!</h3>');
+  res.send('<h3>Go to <a href="https://github.com/soroushchehresa/github-readme-linkedin">github-readme-linkedin</a> for more info!</h3>');
 }
