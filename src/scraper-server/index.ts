d7712c85e305b7c3a75e681d30be67414f8b1dbb
soroushchehresa <s1996ch@gmail.com>
2020-08-22 13:47:31 +0430

    HOTFIX

--- src/scraper-server/index.ts
+++ src/scraper-server/index.ts
@@ -1,21 +1,20 @@
 import { Request, Response } from 'express';
-// import _ from 'lodash';
-// import scraper from './scraper';
+import _ from 'lodash';
+import scraper from './scraper';
 
 export default async (req: Request, res: Response) => {
-  res.json({ error: 'A LinkedIn username is required.' });
-  // if (_.get(req, ['query', 'username'])) {
-  //   try {
-  //     const result = await scraper(`https://www.linkedin.com/in/${_.get(req, ['query', 'username'])}`);
-  //     if (!result) {
-  //       res.json({ error: 'The people/company has not been found.', result: null });
-  //     } else {
-  //       res.json({ error: null, result });
-  //     }
-  //   } catch (e) {
-  //     res.json({ error: e.message, result: null });
-  //   }
-  // } else {
-  //   res.json({ error: 'A LinkedIn username is required.' });
-  // }
+  if (_.get(req, ['query', 'username'])) {
+    try {
+      const result = await scraper(`https://www.linkedin.com/in/${_.get(req, ['query', 'username'])}`);
+      if (!result) {
+        res.json({ error: 'The people/company has not been found.', result: null });
+      } else {
+        res.json({ error: null, result });
+      }
+    } catch (e) {
+      res.json({ error: e.message, result: null });
+    }
+  } else {
+    res.json({ error: 'A LinkedIn username is required.' });
+  }
 }
